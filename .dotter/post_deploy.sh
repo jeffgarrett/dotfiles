5d621a61994146d02647a62778e8477f2e3ba56a
Jeff Garrett <jeff@jgarrett.org>
2022-09-19 13:18:24 -0500

    updates

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,25 +1,12 @@
 set -ex
 
-# Install or update rustup
-rustup_dir="${HOME}/.cargo/bin"
-rustup="${rustup_dir}/rustup"
-
-if ! [ -x "${rustup}" ]; then
-  echo "installing rustup"
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-else
-  echo "updating rustup"
-  "${rustup}" self update
-fi
-
-# Install or update toolchains
-rustup set profile default
-rustup component add miri rust-analyzer
-rustup toolchain install stable nightly
-rustup default nightly
-
 # Cargo-installable utilities
 cargo install --quiet cargo-nextest
 cargo install --quiet fd-find
 cargo install --quiet ripgrep
 cargo install --quiet stylua
+
+# wezterm
+brew tap wez/wezterm
+brew install --cask wez/wezterm/wezterm-nightly
+brew upgrade --cask wezterm-nightly --no-quarantine --greedy-latest

c96d93e419739aeb3bd32694991ffdfa43923ed0
Jeff Garrett <jeff@jgarrett.org>
2023-10-29 10:03:49 -0500

    update

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -29,7 +29,7 @@ config.colors = {
     new_tab = {
       fg_color = colors.foreground,
       bg_color = colors.background,
-    }
+    },
   },
 }
 config.enable_scroll_bar = true
@@ -140,44 +140,3 @@ wezterm.on("update-status", function(window, pane)
 end)
 
 return config
--- return {
---   unix_domains = {
---     {
---       name = "unix",
---     },
---   },
---   window_padding = {
---     left = 0,
---     right = 0,
---     top = 0,
---     bottom = 0,
---   },
--- }
-
--- # Basic status bar colors
--- # Left side of status bar
--- set -g status-left-style fg=colour243,bg=white
--- set -g status-left-length 40
--- set -g status-left "#[fg=white,bg=colour61,nobold] #S #[fg=colour61,bg=white]"
--- # Window status
--- set -g window-status-format "  #I #W  "
--- set -g window-status-current-format "#[fg=white,bg=colour33]#[fg=white,nobold] #I #W #[fg=colour33,bg=white,nobold]"
--- # Window with activity status
--- set -g window-status-activity-style fg=white,bg=colour243
--- # Window separator
--- set -g window-status-separator ""
--- # Window status alignment
--- set -g status-justify left
--- alacritty customizations
---   visual bell: 100ms
---   color scheme: solarized light
---   decorations: none
---   dimensions: 150x50
--- color_scheme = 'Batman',
--- window_frame = {
---   font = wezterm.font "FiraCode Nerd Font",
---   font_size = 14,
--- },
--- set -g status-fg colour245 # base1
-    -- # Current window status
-    -- set -g window-status-current-style fg=white,bg=colour33
